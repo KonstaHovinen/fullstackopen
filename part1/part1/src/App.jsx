2b6c12875b82002fbc5d2b92ff4d40134b975577
Konsta Hovinen <konsta.hovinen@turkuamk.fi>
2025-12-17 10:06:54 +0200

    1.7 done 15 min of work

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = (good - bad ) / all
+  const positive = (good/all) * 100
 
   return (
     <div>
@@ -17,10 +20,13 @@ const App = () => {
         
       
       <h1>statistics</h1>
-      
+
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all</p>
+      <p>average</p>
+      <p>positive</p>
     </div>
   )
 }
