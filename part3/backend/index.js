986e00c2ab1ffa2f6818c8ef60cc0268ec6bd1cd
Konsta Hovinen <konsta.hovinen@turkuamk.fi>
2025-12-21 15:08:55 +0200

    3.2 complete

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -10,3 +10,7 @@ app.use(morgan('tiny'))
 let persons = []  // empty or initial data
 
 app.listen(3001, () => console.log('Server running on port 3001'))
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
